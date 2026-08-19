9e9fae1ef8b83dfa43a25eae4f925d3bc509cdf4
Lam Jun Han <junhanlam0@gmail.com>
2026-08-20 00:26:26 +0800

    Added food

---
+++ src/Food.cpp
@@ -0,0 +1,37 @@
+#include "Food.hpp"
+
+
+Food::Food (FoodType type){
+    m_body = {static_cast<float>(GetRandomValue(0,GetScreenWidth()-30)),-30,30,30};
+    m_speed = 500.0f;
+    m_type = type;
+    m_brocolli = LoadTexture("assets/brocolli.png");
+    m_rotiCanai = LoadTexture("assets/roti-canai.png");
+}
+
+
+void Food::update(){
+    m_body.y += m_speed * GetFrameTime();
+}
+
+void Food::draw(){
+    if (m_type == FoodType::Healthy){
+        DrawTextureEx(m_brocolli,{m_body.x,m_body.y},0.0f,0.1f, WHITE);
+    } else {
+        DrawTextureEx(m_rotiCanai,{m_body.x,m_body.y},0.0f,0.1f, WHITE);
+    }
+}
+
+Rectangle Food::getBounds() const {
+    return m_body;
+}
+
+
+
+FoodType Food::getType() const {
+    return m_type;
+}
+
+bool Food::isOutOfBounds() const {
+    return m_body.y > GetScreenHeight();
+}
---
+++ src/Food.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "raylib.h"
+#include <cstdio>
+
+enum class FoodType{
+    Healthy,
+    Unhealthy
+};
+
+class Food{
+
+public:
+    Food(FoodType type);
+
+    void update();
+    void draw();
+    void reset();
+
+    Rectangle getBounds() const;
+    FoodType getType() const;
+    bool isOutOfBounds() const;
+
+private:
+    Rectangle m_body;
+    float m_speed;
+    FoodType m_type;
+    Texture2D m_brocolli;
+    Texture2D m_rotiCanai;
+    
+};
